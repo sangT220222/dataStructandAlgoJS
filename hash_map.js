b6a9618ef529964d91389307b64547d440a299d4
sangT220222 <magnas9@hotmail.co.uk>
2024-05-07 20:01:05 +0100

    get(), has(), remove() functions have been  added

--- hash_map.js
+++ hash_map.js
@@ -25,12 +25,47 @@ class HashMap {
     if (this.hashMap[hashedKey] !== undefined) {
       console.log(`Collision detected for ${key}`);
     }
-
+    //for now this has been set to overwrite the existing value with the new value for now
     this.hashMap[hashedKey] = { key, value };
+  }
+
+  get(key) {
+    try {
+      const hashedKey = this.hash(key);
+      return this.hashMap[hashedKey].value;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  has(key) {
+    const hashedKey = this.hash(key);
+
+    return this.hashMap[hashedKey] !== undefined;
+  }
+
+  remove(key) {
+    const hashedKey = this.hash(key);
+    if (this.hashMap[hashedKey] === undefined) {
+      return false;
+    }
+    delete this.hashMap[hashedKey];
+    return "Key has been deleted!";
   }
 }
 
+// length() returns the number of stored keys in the hash map.
+
+// clear() removes all entries in the hash map.
+
+// keys() returns an array containing all the keys inside the hash map.
+
+// values() returns an array containing all the values.
+
 const testingMap = new HashMap(10); //creating a new hashmap
-testingMap.set(8, "old value");
-testingMap.set(8, "new value");
+testingMap.set(8, "value1");
+testingMap.set(69, "value2");
+console.log(testingMap);
+console.log(testingMap.has(8));
+console.log(testingMap.remove(3443));
 console.log(testingMap);
